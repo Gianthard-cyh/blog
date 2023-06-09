e781d665d0bd2dd49a53061459142290ed09dcbc
Gianthard-cyh <2953427626@qq.com>
2023-06-09 14:01:36 +0800

    🔧 fix scroll bar 🔧

--- components/PostList.vue
+++ components/PostList.vue
@@ -2,10 +2,10 @@
   <div class="px8">
     <ContentList v-slot="{ list }">
       <a class="group hover:cursor-pointer" v-for="i in list" @click="navigateTo(i._path)">
-        <div class="pt min-w-sm  text-3xl text-white op-50 group-hover:op-100 font-600 transition-all-200">
+        <div class="pt min-w-xs  text-3xl text-white op-50 group-hover:op-100 font-600 transition-all-200">
           {{ i.title }}
         </div>
-        <div class="pt3 min-w-sm text-xl text-white op-30 group-hover:op-90 font-500 transition-all-200">
+        <div class="pt3 min-w-xs text-xl text-white op-30 group-hover:op-90 font-500 transition-all-200">
           {{ i.description }}
         </div>
       </a>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ const route = useRoute()
 <template>
   <div class="bg-truegray-900 h100vh flex flex-col flex-items-center">
     <div class="flex flex-col flex-items-start px8">
-      <div class="pt16 min-w-sm px8 max-w-6xl text-4xl text-white font-900 sm:min-w-xl lg:min-w-2xl xl:min-w-5xl">
+      <div class="pt16 min-w-10 px8 max-w-6xl text-4xl text-white font-900 sm:min-w-xl lg:min-w-2xl xl:min-w-5xl">
         欢迎，
         <br>
         这是 Gianthard 的博客
